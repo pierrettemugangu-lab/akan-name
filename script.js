238a434ca78f8f5416702c2126f609cfdd445422
pierrettemugangu-lab <305752596+pierrettemugangu-lab@users.noreply.github.com>
2026-08-01 23:03:45 +0300

    Refactor event listener for submit button

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ document.getElementById('submit-btn').addEventListener('click', function () {
 
   // 3. Extract CC (Century) and YY (Year) for the formula:
   
-  const CC = Math.floor(year / 100); // First two digits 
+  const CC = Math.floor(year / 100); 
   const YY = year % 100;             // Last two digits 
   const DD = day;
   const MM = month;
